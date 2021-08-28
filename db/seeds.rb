e7276db92767d9121921c24941be65261ad11b55
Bl4z3rr <karnath.bart@gmail.com>
2021-08-28 20:52:29 +0200

    add skills in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Skill.destroy_all
+
+skill = Skill.create(name: "Javascript")
+skill = Skill.create(name: "Python")
+skill = Skill.create(name: "Java")
+skill = Skill.create(name: "C++")
+skill = Skill.create(name: "Swift")
+skill = Skill.create(name: "TypeScript")
+skill = Skill.create(name: "Go Programming Language")
+skill = Skill.create(name: "SQL")
+skill = Skill.create(name: "Ruby on Rails")
+skill = Skill.create(name: "PHP")
+skill = Skill.create(name: "Kotlin")
+skill = Skill.create(name: "C#")
+skill = Skill.create(name: "Rust")
+skill = Skill.create(name: "Scala")
+skill = Skill.create(name: "Elixir")
+skill = Skill.create(name: "HTML")
+skill = Skill.create(name: "CSS")
