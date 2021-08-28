5d4a2b7097d4d3d68fe7e14ae4d5b2bd4cfa1d49
Bl4z3rr <karnath.bart@gmail.com>
2021-08-28 09:43:43 +0200

    add hr_repreentatives controller and view

---
+++ app/controllers/hr_representatives_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class HrRepresentativesController < ApplicationController
+  def about
+    # HrRepresentatives.new(name: 'Bob', surname: 'Zit', email: 'bob@bob.com')  
+  end
+end
